e2c33bb875f905ac3fd0e30852abdfb055cc534f
Zhe Yu <aaazhe825@gmail.com>
2023-12-07 13:04:25 -0500

    Create A3.py

---
+++ A3.py
@@ -0,0 +1,10 @@
+
+### 1. Complete the active learning code
+
+- Complete the three incomplete functions in [src/metrics.py](src/metrics.py).
+
+### 2. Evaluate biases/preferences of your current model
+- Execute [A3.py](src/A3.py) file.
+- Take a screenshot of the execution result (last 10 lines at least).
+- Save the screenshot as _/screenshots/A3.png_.
+- Remember to push your code.
