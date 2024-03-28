d89c31696292b84ce5cbdc1dace52951722d42d3
Shiv Murthy <sm2678@rit.edu>
2024-03-28 18:36:05 -0400

    implemented metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -17,6 +17,14 @@ class Metrics:
         # TPR = #(y=1, y_pred=1) / #(y=1)
         # Write your code below:
 
+        target_bool = np.array(self.y) == 1
+        predicted_bool = np.array(self.y_pred) == 1 
+        senstive_bool_0 = np.array(s) == 0
+        senstive_bool_1 = np.array(s) == 1
+
+        tpr1 = np.sum(target_bool & predicted_bool & senstive_bool_1)/np.sum(target_bool & senstive_bool_1)
+        tpr0 = np.sum(target_bool & predicted_bool & senstive_bool_0)/np.sum(target_bool & senstive_bool_0)
+
         return tpr1-tpr0
 
     def aod(self, s):
@@ -27,7 +35,19 @@ class Metrics:
         # FPR = #(y=0, y_pred=1) / #(y=0)
         # Write your code below:
 
-        return (tpr1 - tpr0 + fpr1 - fpr0)/2.0
+        target_bool_0 = np.array(self.y) == 0
+        target_bool_1 = np.array(self.y) == 1
+        predicted_bool = np.array(self.y_pred) == 1 
+        senstive_bool_0 = np.array(s) == 0
+        senstive_bool_1 = np.array(s) == 1        
+
+        tpr1 = np.sum(target_bool_1 & predicted_bool & senstive_bool_1)/np.sum(target_bool_1 & senstive_bool_1)
+        tpr0 = np.sum(target_bool_1 & predicted_bool & senstive_bool_0)/np.sum(target_bool_1 & senstive_bool_0)
+
+        fp_r1 = np.sum(target_bool_0 & predicted_bool & senstive_bool_1)/np.sum(target_bool_0 & senstive_bool_1) 
+        fp_r0 = np.sum(target_bool_0 & predicted_bool & senstive_bool_0)/np.sum(target_bool_0 & senstive_bool_0) 
+
+        return (tpr1 - tpr0 + fp_r1 - fp_r0)/2.0
 
     def spd(self, s):
         # Statistical Parity Difference
@@ -35,6 +55,14 @@ class Metrics:
         # return: SPD = |PR(s=1)-PR(s=0)|
         # PR = #(y_pred=1) / #y
         # Write your code below:
+
+        target_bool_1 = np.array(self.y) == 1
+
+        senstive_bool_0 = np.array(s) == 0
+        senstive_bool_1 = np.array(s) == 1        
+
+        pr1 = np.sum(target_bool_1 & senstive_bool_1)/np.sum(senstive_bool_1)
+        pr0 = np.sum(target_bool_1 & senstive_bool_0)/np.sum(senstive_bool_0)
 
         return pr1 - pr0
 
