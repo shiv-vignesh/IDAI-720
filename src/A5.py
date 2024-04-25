1a3647fc05e1cf13a54d963cc6eb69d5919a17eb
Shiv Murthy <sm2678@rit.edu>
2024-04-25 17:03:11 -0400

    implemented output_grad

--- src/A5.py
+++ src/A5.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from pdb import set_trace
 
 # Load the previous trained model
-model = VGG_Pre("./checkpoint/attractiveness.keras")
+model = VGG_Pre("./checkpoint/attractiveness.x")
 
 # Load test data
 test, protected = load_scut("../data/test.csv")
